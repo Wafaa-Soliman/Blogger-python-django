3d6cd4e26600166aca42825820c832d565d59ce9
Wafaa Soliman <eng.wafaa.soliman@gmail.com>
2020-10-03 16:18:46 +0200

    first commit

--- src/my_blog/urls.py
+++ src/my_blog/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
 ]
