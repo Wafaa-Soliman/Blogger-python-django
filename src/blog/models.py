a65e89e86bfa597c1e2a4f513e836aa8ef9de647
Wafaa Soliman <eng.wafaa.soliman@gmail.com>
2020-10-05 14:24:22 +0200

    Register Model and Add posts with shell Api

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    post_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
+posts=[
+    {
+        'title': 'التدوينة الأولي',
+        'content': 'نص التدوينة الأولي كنص تجريبي',
+        'post_date': '10-10-2020',
+        'author': 'Wafaa Soliman'
+    },
+    {
+        'title': 'التدوينة الثانية',
+        'content': 'نص التدوينة الثانية كنص تجريبي',
+        'post_date': '15-10-2020',
+        'author': 'Wafaa Soliman'
+    },
+    {
+        'title': 'التدوينة الثالثة',
+        'content': 'نص التدوينة الثالثة كنص تجريبي',
+        'post_date': '20-10-2020',
+        'author': 'Wafaa Soliman'
+    },
+    {
+        'title': 'التدوينة الرابعة',
+        'content': 'نص التدوينة الرابعة كنص تجريبي',
+        'post_date': '25-10-2020',
+        'author': 'Wafaa Soliman'
+    },
+    {
+        'title': 'التدوينة الخامسة',
+        'content': 'نص التدوينة الخامسة كنص تجريبي',
+        'post_date': '4-10-2020',
+        'author': 'Wafaa Soliman'
+    },
+]
+
 def home(request):
-    return render(request,'blog/index.html', {'title': 'Home'})
+    context = {
+        'title': 'الصفحة الرئيسية',
+        'posts': posts
+    }
+    return render(request,'blog/index.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'من أنا'})
