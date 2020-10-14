7bd90a9d75e9c1b64f08019778fd4976f30d3721
Wafaa Soliman <eng.wafaa.soliman@gmail.com>
2020-10-14 20:58:33 +0200

    Install and using crispy forms

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
